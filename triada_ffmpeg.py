40bb6ecffeb9b2863ea015164c6446aaa0290779
Bagrat Dabaghyan <dbagrat@gmail.com>
2023-05-17 21:04:35 +0400

    Impl basic params/functionality
    Input video file or image sequence
    Optional alternative audio input
    Encoding quality (CRF)
    x264 or x265 codec selector
    8 bit or 10 bit (yuv420 or yuv420p10)

---
+++ triada_ffmpeg.py
@@ -0,0 +1,106 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QLabel, QComboBox, QLineEdit, QRadioButton
+import ffmpeg
+
+
+class FFmpegGUI(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle('Triada FFmpeg GUI')
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(QLabel('Video or Image Sequence'))
+        self.video_input = QLineEdit()
+        self.video_button = QPushButton('Browse')
+        self.video_button.clicked.connect(self.select_video)
+        video_layout = QHBoxLayout()
+        video_layout.addWidget(self.video_input)
+        video_layout.addWidget(self.video_button)
+        layout.addLayout(video_layout)
+
+        layout.addWidget(QLabel('Alternative Audio Source (optional)'))
+        self.audio_input = QLineEdit()
+        self.audio_button = QPushButton('Browse')
+        self.audio_button.clicked.connect(self.select_audio)
+        audio_layout = QHBoxLayout()
+        audio_layout.addWidget(self.audio_input)
+        audio_layout.addWidget(self.audio_button)
+        layout.addLayout(audio_layout)
+
+        layout.addWidget(QLabel('Encoding Quality (CRF)'))
+        self.crf_input = QLineEdit()
+        layout.addWidget(self.crf_input)
+
+        layout.addWidget(QLabel('Codec'))
+        self.codec_combo = QComboBox()
+        self.codec_combo.addItems(['libx264', 'libx265'])
+        layout.addWidget(self.codec_combo)
+
+        layout.addWidget(QLabel('Pixel Format'))
+        self.pix_fmt_8bit = QRadioButton('8-bit (yuv420p)')
+        self.pix_fmt_10bit = QRadioButton('10-bit (yuv420p10le)')
+        layout.addWidget(self.pix_fmt_8bit)
+        layout.addWidget(self.pix_fmt_10bit)
+        self.pix_fmt_8bit.setChecked(True)
+
+        self.encode_button = QPushButton('Encode')
+        self.encode_button.clicked.connect(self.encode_video)
+        layout.addWidget(self.encode_button)
+
+        self.setLayout(layout)
+
+    def select_video(self):
+        video_file, _ = QFileDialog.getOpenFileName()
+        if video_file:
+            self.video_input.setText(video_file)
+
+    def select_audio(self):
+        audio_file, _ = QFileDialog.getOpenFileName()
+        if audio_file:
+            self.audio_input.setText(audio_file)
+
+    def encode_video(self):
+        video_file = self.video_input.text()
+        audio_file = self.audio_input.text()
+        crf = self.crf_input.text()
+        codec = self.codec_combo.currentText()
+        pix_fmt = 'yuv420p' if self.pix_fmt_8bit.isChecked() else 'yuv420p10le'
+
+        if not video_file or not crf:
+            return
+
+        output_file = f"output_{codec}_{pix_fmt}.mkv"
+
+        if '%' in video_file:
+            input_stream = ffmpeg.input(video_file, format='image2', framerate=25)
+        else:
+            input_stream = ffmpeg.input(video_file)
+
+        try:
+            if audio_file:
+                audio_stream = ffmpeg.input(audio_file)
+                audio = audio_stream.audio
+                video = input_stream.video
+                stream = ffmpeg.concat(video, audio, v=1, a=1)
+            else:
+                stream = input_stream
+
+            (
+                stream
+                .output(output_file, vcodec=codec, crf=crf, pix_fmt=pix_fmt)
+                .run(capture_stdout=True, capture_stderr=True)
+            )
+
+        except ffmpeg.Error as e:
+            print(e.stderr.decode())
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = FFmpegGUI()
+    window.show()
+    sys.exit(app.exec_())
