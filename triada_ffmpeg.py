a1547e2ace8453514a28388a7996263b7e474c14
Bagrat Dabaghyan <dbagrat@gmail.com>
2023-05-17 23:26:11 +0400

    Make the Quality/CRF param a slider Change output file extension to mp4

--- triada_ffmpeg.py
+++ triada_ffmpeg.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QLabel, QComboBox, QLineEdit, QRadioButton
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QLabel, QComboBox, QLineEdit, QRadioButton, QSlider
+from PyQt5.QtCore import Qt
 import ffmpeg
 
 
@@ -32,10 +33,6 @@ class FFmpegGUI(QWidget):
         audio_layout.addWidget(self.audio_button)
         layout.addLayout(audio_layout)
 
-        layout.addWidget(QLabel('Encoding Quality (CRF)'))
-        self.crf_input = QLineEdit()
-        layout.addWidget(self.crf_input)
-
         layout.addWidget(QLabel('Codec'))
         self.codec_combo = QComboBox()
         self.codec_combo.addItems(['libx264', 'libx265'])
@@ -48,11 +45,25 @@ class FFmpegGUI(QWidget):
         layout.addWidget(self.pix_fmt_10bit)
         self.pix_fmt_8bit.setChecked(True)
 
+        self.crf_label = QLabel()
+        layout.addWidget(self.crf_label)
+        self.crf_slider = QSlider(Qt.Horizontal)
+        self.crf_slider.setRange(1, 32)
+        self.crf_slider.setValue(16)
+        self.crf_slider.setTickPosition(QSlider.TicksBelow)
+        self.crf_slider.setTickInterval(1)
+        self.crf_slider.valueChanged.connect(self.update_crf_label)
+        layout.addWidget(self.crf_slider)
+        self.update_crf_label(self.crf_slider.value())
+
         self.encode_button = QPushButton('Encode')
         self.encode_button.clicked.connect(self.encode_video)
         layout.addWidget(self.encode_button)
 
         self.setLayout(layout)
+
+    def update_crf_label(self, value):
+        self.crf_label.setText(f"Quality (CRF): {value}")
 
     def select_video(self):
         video_file, _ = QFileDialog.getOpenFileName()
@@ -67,14 +78,14 @@ class FFmpegGUI(QWidget):
     def encode_video(self):
         video_file = self.video_input.text()
         audio_file = self.audio_input.text()
-        crf = self.crf_input.text()
+        crf = self.crf_slider.value()
         codec = self.codec_combo.currentText()
         pix_fmt = 'yuv420p' if self.pix_fmt_8bit.isChecked() else 'yuv420p10le'
 
-        if not video_file or not crf:
+        if not video_file:
             return
 
-        output_file = f"output_{codec}_{pix_fmt}.mkv"
+        output_file = f"output_{codec}_{pix_fmt}.mp4"
 
         if '%' in video_file:
             input_stream = ffmpeg.input(video_file, format='image2', framerate=25)
