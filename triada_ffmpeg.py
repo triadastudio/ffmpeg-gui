4f915f61e78520c9fa7295a0e2dad932972df0db
Bagrat Dabaghyan <dbagrat@gmail.com>
2023-05-18 06:16:40 +0400

    Impl progress indication, run encoder in separate thread
    Impl get_frame_count function that works for a video file and an image sequence
    Impl EncoderThread class
    Add progress bar widget

--- triada_ffmpeg.py
+++ triada_ffmpeg.py
@@ -1,7 +1,34 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QLabel, QComboBox, QLineEdit, QRadioButton, QSlider
-from PyQt5.QtCore import Qt
+import re
+import subprocess
+import glob
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QLabel, QComboBox, QLineEdit, QRadioButton, QSlider, QProgressBar
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 import ffmpeg
+
+
+class EncoderThread(QThread):
+    progress = pyqtSignal(int)
+
+    def __init__(self, cmd):
+        super().__init__()
+        self.cmd = cmd
+
+    def run(self):
+        process = subprocess.Popen(self.cmd,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT,
+                                   universal_newlines=True)
+
+        for line in process.stdout:
+            print(line.strip())
+            progress_match = re.search(r'frame=\s*(\d+)', line)
+
+            if progress_match:
+                frame_number = int(progress_match.group(1))
+                self.progress.emit(frame_number)
+
+        process.wait()
 
 
 class FFmpegGUI(QWidget):
@@ -60,6 +87,9 @@ class FFmpegGUI(QWidget):
         self.encode_button.clicked.connect(self.encode_video)
         layout.addWidget(self.encode_button)
 
+        self.progress_bar = QProgressBar()
+        layout.addWidget(self.progress_bar)
+
         self.setLayout(layout)
 
     def update_crf_label(self, value):
@@ -75,6 +105,28 @@ class FFmpegGUI(QWidget):
         if audio_file:
             self.audio_input.setText(audio_file)
 
+    @staticmethod
+    def get_frame_count(file_path):
+        try:
+            frame_count = 0
+
+            # Check if the input is an image sequence
+            if '%' in file_path:
+                # Replace the %0xd part of the pattern with a wildcard
+                pattern = re.sub(r'%0\d+d', '*', file_path)
+                # Count the number of matching files in the directory
+                frame_count = len(glob.glob(pattern))
+            else:
+                streams = ffmpeg.probe(file_path)["streams"]
+                for stream in streams:
+                    if stream['codec_type'] == 'video':
+                        frame_count = int(stream['nb_frames'])
+
+            return frame_count
+
+        except (ffmpeg.Error, KeyError, StopIteration):
+            return None
+
     def encode_video(self):
         video_file = self.video_input.text()
         audio_file = self.audio_input.text()
@@ -88,7 +140,8 @@ class FFmpegGUI(QWidget):
         output_file = f"output_{codec}_{pix_fmt}.mp4"
 
         if '%' in video_file:
-            input_stream = ffmpeg.input(video_file, format='image2', framerate=25)
+            input_stream = ffmpeg.input(
+                video_file, format='image2', framerate=25)
         else:
             input_stream = ffmpeg.input(video_file)
 
@@ -101,14 +154,41 @@ class FFmpegGUI(QWidget):
             else:
                 stream = input_stream
 
-            (
-                stream
-                .output(output_file, vcodec=codec, crf=crf, pix_fmt=pix_fmt)
-                .run(capture_stdout=True, capture_stderr=True)
+            frame_count = self.get_frame_count(video_file)
+            if frame_count is not None:
+                self.progress_bar.setMaximum(frame_count)
+
+            cmd = (
+                ffmpeg
+                .compile(
+                    stream
+                    .output(output_file,
+                            vcodec=codec,
+                            crf=crf,
+                            pix_fmt=pix_fmt,
+                            y=None)
+                )
             )
+
+            print("FFmpeg command:", " ".join(cmd))
+
+            self.encoder_thread = EncoderThread(cmd)
+            self.encoder_thread.progress.connect(self.update_progress)
+            self.encoder_thread.finished.connect(self.encoding_finished)
+            self.encoder_thread.start()
+            self.encode_button.setEnabled(False)
 
         except ffmpeg.Error as e:
             print(e.stderr.decode())
+
+    def update_progress(self, frame_number):
+        self.progress_bar.setValue(frame_number)
+
+    def encoding_finished(self):
+        self.progress_bar.reset()
+        self.encode_button.setEnabled(True)
+        print("Encoding finished")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
